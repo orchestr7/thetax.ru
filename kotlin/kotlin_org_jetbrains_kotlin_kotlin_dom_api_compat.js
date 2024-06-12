4aa525c369a4c87ada92d1e190c1f9fea04429db
akuleshov7 <andrewkuleshov7@gmail.com>
2024-06-12 20:28:46 +0000

    Deploying to gh-pages from  @ 1b8c6c367b82519cdc23c027ca9033f50b7f30f6 🚀

--- kotlin/kotlin_org_jetbrains_kotlin_kotlin_dom_api_compat.js
+++
@@ -1,8 +0,0 @@
-(function (_) {
-  'use strict';
-  //region block: pre-declaration
-  //endregion
-  return _;
-}(module.exports));
-
-//# sourceMappingURL=kotlin_org_jetbrains_kotlin_kotlin_dom_api_compat.js.map
